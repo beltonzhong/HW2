6b9656fd9437bde6f5f04404200d44e2d6b75683
beltonzhong <byzhong@ucsd.edu>
2017-10-23 15:24:03 -0700

    asd

--- hw2.m
+++ hw2.m
@@ -26,52 +26,46 @@ hidden_layer_units = 128;
 w_hidden = rand(785, hidden_layer_units);
 w_output = rand(hidden_layer_units, 10);
 
-image = images(:, 1);
+for counter = 1:20000
+    image = images(:, counter);
 
-a_hidden = w_hidden' * image;
+    a_hidden = w_hidden' * image;
 
-g_hidden = 1 ./ (1 + exp(a_hidden * -1));
+    g_hidden = 1 ./ (1 + exp(a_hidden * -1));
 
-a_output = g_hidden' * w_output;
+    a_output = g_hidden' * w_output;
+    
+    sumexp = sum(exp(a_output));
+    g_output = exp(a_output) ./ sumexp;
 
-g_output = a_output;
-g_output(g_output < 0) = 0;
+    [value, indices] = max(g_output');
+    predictions = indices;
+    y = zeros(10, 1);
+    for i=1:10
+        y(i, predictions == i) = 1;
+    end
 
-[value, indices] = max(g_output');
-predictions = indices;
-y = zeros(10, 1);
-for i=1:10
-    y(i, predictions == i) = 1;
-end
+    y = y';
+    label = labels1hot(counter, :);
+    delta_output = label - y;
 
-y = y';
-label = labels1hot(1, :);
-delta_output = label - y;
+    delta_hidden = zeros(hidden_layer_units, 1);
+    for i=1:hidden_layer_units
+        a = a_hidden(i);
+        grad = exp(-a) / ((1 + exp(-a)) ^ 2);
+        delta_hidden(i) = sum(w_output(i, :) .* delta_output) * grad;
+    end
 
+    alpha = .1;
+    for i=1:hidden_layer_units
+        for j=1:10
+            w_output(i, j) = w_output(i, j) + alpha * delta_output(j) * g_hidden(i);
+        end
+    end
 
-syms a;
-activ_func = 1 / (1 + exp(-a));
-gradient_func = diff(activ_func);
-
-delta_hidden = zeros(128, 1);
-for i=1:128  
-    a = a_hidden(i);
-    delta_hidden(i) = sum(w_output(i, :) .* delta_output) * eval(gradient_func);
-end
-
-alpha = .1;
-for i=1:128
-    for j=1:10
-        w_output(i, j) = w_output(i, j) + alpha * delta_output(j) * g_hidden(i);
+    for i = 1:758
+        for j = 1: 128
+            w_hidden(i, j) = w_hidden(i, j) + alpha * delta_hidden(j) * image(i);
+        end
     end
 end
-
-for i = 1:758
-    for j = 1: 128
-        w_hidden(i, j) = w_hidden(i, j) + alpha * delta_hidden(j) * image(i);
-    end
-end
-
-
-
-
